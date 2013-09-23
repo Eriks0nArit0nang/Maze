9a62f24bc9d684d649c567acc9042e4e94e3daf9
James Loewen <james.loewen@uwaterloo.ca>
2013-09-23 14:15:08 -0400

    Update Multiplayer mode
    Fix issue regarding closing when playing multiplayer mode.

--- Multiplayer.h
+++ Multiplayer.h
@@ -5,6 +5,7 @@ Player multiplayer ()
        cin >> n;
        Player players[n];
        int best_level[n];
+       int winner; // will store the index of the last player to play a level
        pair <int[4],int[GRID_SIZE][GRID_SIZE]> level;
        string t;
        for (int i = 0; i < 100; i++)
@@ -18,15 +19,27 @@ Player multiplayer ()
            set_background (level.second);
            for (int j = 0; j < n; j++)
            {
+               if (close_button_pressed || key[KEY_ESC])
+                  break;
+                                        
                rectfill (map,0,0,GRID_SIZE*2,600,makecol(100,100,100));
                if (players[j].Get_Health() > 0)
                {
                   best_level[j] = i+1;
+                  winner = j;
                   clear_keybuf();
                   cout << "Press any key to continue player " << j+1;
                   rest(100);
                   players[j]=play_level (level,players[j],(i+1)/2);
                }
            }
+           if (close_button_pressed || key[KEY_ESC])
+              break;
        }
+       if (close_button_pressed || key[KEY_ESC])
+          return players[winner];
+       for (int j = 0; j < n; j++)
+           printf ("Player %d reached Level %d before dying.\n", j, best_level[j]);
+       printf ("Player %d is the winner\n", winner);
+       return players[winner];
 }
